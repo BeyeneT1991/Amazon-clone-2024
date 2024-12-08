c1b225c0f668d23f2ae3056b8e4d96b7741e0385
BeyeneT1991 <eseynahom8@gmail.com>
2024-12-08 11:40:24 -0600

    Frontend

--- amazone-clone/src/API/axios.js
+++ amazone-clone/src/API/axios.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://127.0.0.1:5001/clone-2024-c5db0/us-central1/api"
+
+    // local instance of firebase function
+    // baseURL: "http://127.0.0.1:5001/clone-2024-c5db0/us-central1/api",
+
+    //deployed version of amazon server on render.com
+    baseURL: "https://amazon-clone-2024-qap0.onrender.com"
 });
 
 
